e7152378cca4ee30349c59d38e83e9c10ab8e6c7
LuAliSora <166391258+LuAliSora@users.noreply.github.com>
2026-03-22 20:53:44 +0800

    utils

--- nets/unet.py
+++ nets/unet.py
@@ -22,7 +22,7 @@ class unetUp(nn.Module):
         return outputs
 
 class Unet(nn.Module):
-    def __init__(self, num_classes = 21, pretrained = False, backbone = 'vgg'):
+    def __init__(self, num_classes, backbone = 'vgg', pretrained = True):
         super(Unet, self).__init__()
         if backbone == 'vgg':
             self.vgg    = VGG16(pretrained = pretrained)
